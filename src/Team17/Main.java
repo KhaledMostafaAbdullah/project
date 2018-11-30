b43671de45c6ac05bd0ffacfc0ed6b69cb361ee1
Mohamed Anwar <moanwar1997@gmail.com>
2018-11-30 16:25:51 +0200

    updating the new main with the latest requirements

--- src/Team17/Main.java
+++ src/Team17/Main.java
@@ -8,26 +8,35 @@ import java.util.*;
 
 public class Main {
 	private static Scanner input;
-	public static void main(String[]args) {
 
+	private static void print_messages() {
+		System.out.println("<<<Main Menu>>>");
+		System.out.println("1-Most repeated value\t2-Sort\t3-Shuffle\t4-Find the largest prime\t5-Find the smallest prime\t6-Check palindrome");
+		System.out.println("7-Check sorted\t8-Count Primes-\t9-Reverse array\t10-Shift array\t11-Distinct array\t12-Get the maximum 3 numbers");
+		System.out.println("13-Get the minimum 3 numbers\t14-Get average\t15-Get median\t16-Return only primes\t17-Zero if less than zero\t18-to execute all");
+		System.out.println("press 0('zero') to exit");
+		System.out.println("Enter the number of which function to be executed:");
+	}
+
+	public static void main(String[] args) {
+
+
+		System.out.println("Enter the size of array:");
 
 		input = new Scanner(System.in);
-		System.out.println("Press 1 to input numbers and 2 for characters");
-		int typeOfArray = input.nextInt();
-		input = new Scanner(System.in); // if i don't re declare it raises some run time error
-		System.out.println("Please Input the Array");
-		String array = input.nextLine();    // get the entire line after the prompt
+		int size = input.nextInt();// this wil be used for both integers and chars array
 
-		if (typeOfArray == 1) {
-			String[] numbers = array.replace(" ", "").split(",");
-			int[] a = new int[numbers.length];
-			for (int i = 0; i < numbers.length; i++) {
-				System.out.println(numbers[i]);
-				a[i] = Integer.parseInt(numbers[i]);
-			}
-		} else {
-			char[] char_array = array.toCharArray(); //i haven't test this well
+		int[] int_arr = new int[size]; char[] char_arr = new char[size]; //declaration of both int and char array
+
+		System.out.println("Fill the array of Type INTEGERS:"); //filling the int array
+		for (int i = 0; i < int_arr.length; ++i) {
+			int_arr[i] = input.nextInt();
 		}
+
+		System.out.println("Please Fill Characters Array"); //filling the char array
+		for (int i = 0; i < char_arr.length; ++i)
+			char_arr[i] = input.next().charAt(0);
+
 
 		print_messages();
 		int choice = input.nextInt();
@@ -42,20 +51,13 @@ public class Main {
 				switch_runner(choice);
 			}
 			print_messages();
-			 choice = input.nextInt();
+			choice = input.nextInt();
 		}
-		
+
 	}
 
-	private static void print_messages(){
-		System.out.println("<<<Main Menu>>>");
-		System.out.println("1-Most repeated value\t2-Sort\t3-Shuffle\t4-Find the largest prime\t5-Find the smallest prime\t6-Check palindrome");
-		System.out.println("7-Check sorted\t8-Count Primes-\t9-Reverse array\t10-Shift array\t11-Distinct array\t12-Get the maximum 3 numbers");
-		System.out.println("13-Get the minimum 3 numbers\t14-Get average\t15-Get median\t16-Return only primes\t17-Zero if less than zero\t18-to execute all");
-		System.out.println("press 0('zero') to exit");
-		System.out.println("Enter the number of which function to be executed:");
-	}
-	private static void switch_runner(int choice){
+
+	private static void switch_runner(int choice) {
 		switch (choice)   //Add your functions
 		{
 			case 1:
@@ -80,6 +82,7 @@ public class Main {
 				break;
 			case 6:
 				//Check palindrome
+				//please call both the integer and char array
 				//please try not to exceed two when calling your class
 				break;
 			case 7:
@@ -96,6 +99,7 @@ public class Main {
 				break;
 			case 10:
 				//Shift array
+				//please call both the integer and char array
 				//please try not to exceed two when calling your class
 				break;
 			case 11:
