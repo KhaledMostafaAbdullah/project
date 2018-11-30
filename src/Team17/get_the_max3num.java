839a7294c04e0a627c6c65d3e2fdcd4d6353f908
ahmedosama9100 <ahmedosama9100@gmail.com>
2018-12-01 00:26:32 +0200

    add max 3 numbers class

--- src/Team17/get_the_max3num.java
+++ src/Team17/get_the_max3num.java
@@ -1,6 +1,6 @@
 package Team17;
 import java.util.Arrays;
-public static class get_the_max3num{
+public class get_the_max3num{
        public static void max_3_num(int arr[]) {
         int []temp_arr = new int [arr.length];
         System.arraycopy(arr, 0, temp_arr, 0, arr.length);
