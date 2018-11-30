ade8839d5c71c0248d680b163b3dadcc7b052c6b
ahmedosama9100 <ahmedosama9100@gmail.com>
2018-11-30 23:03:19 +0200

    adding get the maximux three numbers class

---
+++ src/Team17/get_the_max3num.java
@@ -0,0 +1,12 @@
+package Team17;
+public static class get_the_max3num{
+       public static void max_3_num(int arr[]) {
+        int []temp_arr = new int [arr.length];
+        System.arraycopy(arr, 0, temp_arr, 0, arr.length);
+        Arrays.sort(temp_arr);
+        for (int i = arr.length - 1 ; i >= arr.length-3 ; i--){
+            System.out.print(temp_arr[i]+" ");
+        }
+        System.out.println("");
+        }
+    }
