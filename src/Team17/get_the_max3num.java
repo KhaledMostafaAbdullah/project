b1fc2a129863aa49fc47625d9e58265cc6733d6d
ahmedosama9100 <[Your>
2018-12-01 20:52:07 +0200

    get max three numbers

--- src/Team17/get_the_max3num.java
+++ src/Team17/get_the_max3num.java
@@ -5,6 +5,7 @@ public class get_the_max3num{
         int []temp_arr = new int [arr.length];
         System.arraycopy(arr, 0, temp_arr, 0, arr.length);
         Arrays.sort(temp_arr);
+	System.out.println("the max 3 numbers : ");
         for (int i = arr.length - 1 ; i >= arr.length-3 ; i--){
             System.out.print(temp_arr[i]+" ");
         }
